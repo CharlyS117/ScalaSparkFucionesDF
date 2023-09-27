9a5e0b37d9136e03e07e5c811429a485f87bba6c
CharlyS117 <115383504+CharlyS117@users.noreply.github.com>
2023-09-27 11:37:20 -0600

    Update Field, add the functionality

--- spark-scala/src/main/scala/com/compiled/fields/Field.scala
+++ spark-scala/src/main/scala/com/compiled/fields/Field.scala
@@ -1,1 +1,12 @@
+package com.compiled.fields
 
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.functions.col
+
+//Trait that can be inherited for use the attributes.
+trait Field {
+
+  val name: String
+  //A lazy is a val that will assess only when call it
+  lazy val column: Column = col(name)
+}
