4e43597af7315ecf01246d43d000a701eba5ca4e
CharlyS117 <115383504+CharlyS117@users.noreply.github.com>
2023-09-28 16:54:09 -0600

    Update StaticVals.scala

--- spark-scala/src/main/scala/com/compiled/common/StaticVals.scala
+++ spark-scala/src/main/scala/com/compiled/common/StaticVals.scala
@@ -1,1 +1,37 @@
+package com.compiled.common
 
+/*
+* Dentro de este .scala lo que se hace es poner aquellos valores
+* que son constantes o estaticos pero no contiene valores muy grandes o mensajes de logs
+* pueden definirse varios case objects
+* */
+object StaticVals {
+  
+  case object CourseConfigConstants {
+  }
+  
+  case object NumericsConstants{
+    val Zero = 0
+  }
+  
+  case object AlfanumericConstanst{
+    val Comma = ","
+  }
+  
+  case object Messages {
+    val WelcomeMessage: String => String = {
+      (devName: String) => s"-----------------------$devName-------------------------------"
+    }
+  }
+  
+  case object JoinTypes {
+    val LeftJoin: String = "left"
+    val OuterJoin: String = "outer"
+    val Inner: String = "inner"
+    val LeftAnti: String = "leftanti"
+  }
+  
+  case object MessageExpections {
+    val JoinExceptionColumns: Array[String] => String ={ (columns: Array[String]) => s"Los campos [${columns.mkString(", ")}] no se encontraron"}
+  }
+}
