cf975448c8ff94f68f8ef5c9540babc850488364
CharlyS117 <115383504+CharlyS117@users.noreply.github.com>
2023-09-28 17:24:20 -0600

    Update StaticVals.scala

--- spark-scala/src/main/scala/com/compiled/common/StaticVals.scala
+++ spark-scala/src/main/scala/com/compiled/common/StaticVals.scala
@@ -8,23 +8,42 @@ package com.compiled.common
 object StaticVals {
   
   case object CourseConfigConstants {
+    /*
+    *In this case object can put info "val´s" that uses in the variables configs
+    */
   }
   
   case object NumericsConstants{
+    /*
+    *In this case object can put info "val´s" that uses in the variables in many
+    *methods
+    */
     val Zero = 0
   }
   
   case object AlfanumericConstanst{
+    /*
+    *In this case object can put info "val´s" that uses in the variables in many
+    *methods
+    */
     val Comma = ","
   }
   
   case object Messages {
+    /*
+    *In this case object can put info "val´s" that uses in the variables in many
+    *methods when want send a messagge on a log.
+    */
     val WelcomeMessage: String => String = {
       (devName: String) => s"-----------------------$devName-------------------------------"
     }
   }
   
   case object JoinTypes {
+    /*
+    *In this case object can put info "val´s" that uses in the variables in many
+    *methods when want use a join in a two DF
+    */
     val LeftJoin: String = "left"
     val OuterJoin: String = "outer"
     val Inner: String = "inner"
@@ -32,6 +51,10 @@ object StaticVals {
   }
   
   case object MessageExpections {
+    /*
+    *In this case object can put info "val´s" that uses in the variables in many
+    *methods when send a messagge on a exception.
+    */
     val JoinExceptionColumns: Array[String] => String ={ (columns: Array[String]) => s"Los campos [${columns.mkString(", ")}] no se encontraron"}
   }
 }
