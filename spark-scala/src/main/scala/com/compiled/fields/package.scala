2b6448d8d12d71263172f29d1284e06ae569013a
CharlyS117 <115383504+CharlyS117@users.noreply.github.com>
2023-09-28 09:32:32 -0600

    Update package.scala

--- spark-scala/src/main/scala/com/compiled/fields/package.scala
+++ spark-scala/src/main/scala/com/compiled/fields/package.scala
@@ -1,1 +1,17 @@
+package com.compiled.field
 
+package object fields {
+  //Case object que funciona como un inicializador de la variable name
+  //Este al igual que los demas hereda razgos del traid Field.
+  
+  case object AvgGlobalSales extends Field {
+    override val name: String = "AVG_GLOBAL_Sales"
+
+    def apply(): Column = {
+      val w: WindowSpec = Window
+        .orderBy(VideoGId.column.desc)
+
+      avg(GlobalSales.column) over w alias name
+    }
+  }
+}
